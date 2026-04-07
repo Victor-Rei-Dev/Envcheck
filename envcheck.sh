124f87d3ed409916fc085c18d86293026a7d73d2
Victor Rei <victorrei.tech@gmail.com>
2026-04-07 18:27:51 -0500

    Adicionando help e parâmetros extras

--- envcheck.sh
+++ envcheck.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-meu_array=("git" "python3" "gcc")
+# Adiciona --help
+if [[ "$1" == "--help" ]]; then
+    echo "Uso: ./envcheck.sh"
+    echo "Verifica se git, python3, gcc estão instalados"
+    exit 0;
+fi
+
+if [[ "$#" -gt 0 ]]; then
+meu_array=("$@")
+else meu_array=("git" "python3" "gcc");
+fi
+
 verificador() {
 for nome in "${meu_array[@]}"; do
 	if command -v "$nome";then echo "${nome} instalado"; 
@@ -7,6 +18,6 @@ for nome in "${meu_array[@]}"; do
         fi 
         done
 
-}
+} 
 
 verificador
